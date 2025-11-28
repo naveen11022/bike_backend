d79f685fc94bb0b58b2249f448a4f3b18e6d4755
Naveen Prasath <naviprasaad50@gmail.com>
2025-11-28 19:11:54 +0530

    Remove backend files

--- app/core/security.py
+++
@@ -1,6 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(
-    schemes=["argon2"],
-    deprecated="auto"
-)
--- app/utils/hashing.py
+++
@@ -1,9 +0,0 @@
-from app.core.security import pwd_context
-
-
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
